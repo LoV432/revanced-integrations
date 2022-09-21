3fef92c5b68432258296c24dc6668cce3cefa870
LoV432 <60856741+LoV432@users.noreply.github.com>
2022-09-21 09:11:04 +0500

    Update SwipeGestureController.kt

--- app/src/main/java/app/revanced/integrations/swipecontrols/controller/gesture/SwipeGestureController.kt
+++ app/src/main/java/app/revanced/integrations/swipecontrols/controller/gesture/SwipeGestureController.kt
@@ -126,7 +126,7 @@ open class SwipeGestureController(
      *
      * @param e the motion event
      */
-    override fun onUp(e: MotionEvent) {
+    open fun onUp(e: MotionEvent) {
         LogHelper.debug(this.javaClass, "onUp(${e.x}, ${e.y}, ${e.action})")
         inSwipeSession = false
         currentSwipe = SwipeDirection.NONE
